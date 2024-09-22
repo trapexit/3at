fe87c15dd99cf7622dd18f9f9fc79793338a4a39
Antonio SJ Musumeci <trapexit@spawn.link>
2024-09-22 14:45:44 -0500

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -39,7 +39,7 @@ generate_to_sdx2_argparser(CLI::App      &app_,
                            Opts::Options &opts_)
 {
   CLI::App *subcmd;
-  Opts::ToADP4 &opts = opts_.to_sdx2;
+  Auto &opts = opts_.to_sdx2;
 
   subcmd = app_.add_subcommand("to-sdx2","");
   subcmd->add_option("filepaths",opts.filepaths)
