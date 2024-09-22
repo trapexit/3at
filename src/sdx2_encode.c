1ca787ca805ccb1991f590a80e4811a81ad1e62c
Antonio SJ Musumeci <trapexit@spawn.link>
2024-09-22 01:58:44 -0500

    sdx2_encode.c

--- src/sdx2_encode.c
+++ src/sdx2_encode.c
@@ -170,7 +170,7 @@ sdx2_encode_mono(const s16 *ibuf_,
         {
           ctx->maxMonoErr = err;
           printf("   (New Max Err = %ld...  Curr:%d, Prev:%d, Comp:%d, Deco:%d) \n",
-                 ctx->maxMonoErr, 
+                 max_err,
                  curr_sample,
                  prev_sample,
                  comp_sample,decode((s32)comp_sample,(s32)prev_sample));
