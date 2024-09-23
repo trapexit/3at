536f2aab03358ba6fb7d4091db32177f19d32aa6
Antonio SJ Musumeci <trapexit@spawn.link>
2024-09-23 18:09:37 -0500

    sdx2_encode.c

--- src/sdx2_encode.c
+++ src/sdx2_encode.c
@@ -107,7 +107,6 @@ encode_sample(s32 curr_sample_,
   exact = set_exact_mode(exact);
 
   tmp = delta_sample(curr_sample_,exact,prev_sample_);
-  //  tmp = abs_s16(curr_sample_ - decode_sample(exact,prev_sample_));
   if(delta_sample(curr_sample_,exact+2,prev_sample_) < tmp)
     exact += 2;
   else if(abs_s16(curr_sample_ - decode_sample(exact-2,prev_sample_)) < tmp)
