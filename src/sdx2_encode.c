da949f328b0bc1ff786bd249e474c2f33d5e8d74
Antonio SJ Musumeci <trapexit@spawn.link>
2024-09-22 23:25:52 -0500

    sdx2_encode.c

--- src/sdx2_encode.c
+++ src/sdx2_encode.c
@@ -180,7 +180,7 @@ sdx2_encode_stereo(const s16 *ibuf_,
 
       if(i)
         {
-          comp_sample = encode_sample((s32)curr_right_sample,(s32)prev_right_sample);
+          comp_sample = encode_sample(curr_right_sample,prev_right_sample);
         }
       else 
         {
