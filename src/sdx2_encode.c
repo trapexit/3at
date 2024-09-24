48719fe7b4fdd8958b31d8cbad39938f954f3e2c
Antonio SJ Musumeci <trapexit@spawn.link>
2024-09-23 19:15:51 -0500

    sdx2_encode.c

--- src/sdx2_encode.c
+++ src/sdx2_encode.c
@@ -94,14 +94,14 @@ delta_sample(const s16 curr_,
   s16 dec_sample;
 
   dec_sample = decode_sample(curr_exact_,prev_);
-  
+
   return abs_s16(curr_ - dec_sample);
 }
 
 
 /*
   See FIG 5 on page 5 of US Patent US005617506A
-  
+
   This code doesn't exactly match but is inspired by the original
   MacOS SquashSnd app. It had some changes that are assumed to be
   relevant.
