2e80236824ea0bb1783185570182ef8a244590b7
Antonio SJ Musumeci <trapexit@spawn.link>
2024-09-23 19:19:36 -0500

    sdx2_encode.c

--- src/sdx2_encode.c
+++ src/sdx2_encode.c
@@ -138,8 +138,7 @@ encode_sample(const s16 curr_sample_,
   /* check for wraparound on the delta case */
   if(tmp > 30000)
     {
-      // we overflowed 16 bits on this delta.
-      // Pull it closer to the center
+      /* Overflowed 16bit on this delta. Pull it closer to the center. */
       delta = ((delta < 0)? (delta + 2) : (delta - 2));
       tmp = delta_sample(curr_sample_,delta,prev_sample_);
     }
