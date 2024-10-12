dd29f00db245d6c3c37e8fdd51bcf0546674ccb2
Antonio SJ Musumeci <trapexit@spawn.link>
2024-10-11 20:48:00 -0500

    intel_dvi_encode2.c

--- src/intel_dvi_encode2.c
+++ src/intel_dvi_encode2.c
@@ -1,40 +1,11 @@
 /*
-  This code is based on two sources:
+  This code is based on the public domain examples found in
+  "Recommended Practices for Enhancing Digital Audio Compatibility in
+  Multimedia Systems" by the IMA Digital Audio Focus and Technical
+  Working Groups Revision 3.00 from 1992-10-21. Specifically Appendix
+  D: Reference Algorithms section 6 "ADPCM Reference Algorithms" on
+  page 28.
 
-  1) https://github.com/Kalmalyzer/adpcm-68k
-
-  2) The public domain examples found in "Recommended Practices for
-  Enhancing Digital Audio Compatibility in Multimedia Systems" by the
-  IMA Digital Audio Focus and Technical Working Groups Revision 3.00
-  from 1992-10-21. Specifically Appendix D: Reference Algorithms
-  section 6 "ADPCM Reference Algorithms" on page 28.
-
-  ========================================================================
-  
-  MIT License
-
-  Copyright (c) 2023 Mikael Kalms
-
-  Permission is hereby granted, free of charge, to any person
-  obtaining a copy of this software and associated documentation files
-  (the "Software"), to deal in the Software without restriction,
-  including without limitation the rights to use, copy, modify, merge,
-  publish, distribute, sublicense, and/or sell copies of the Software,
-  and to permit persons to whom the Software is furnished to do so,
-  subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be
-  included in all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-  BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-  ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  SOFTWARE.
-  
   ========================================================================
 
   ISC License
