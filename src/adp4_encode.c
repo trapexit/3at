06513ba315df451cbad765f1f435c3914b449416
Antonio SJ Musumeci <trapexit@spawn.link>
2024-10-12 15:37:46 -0500

    adp4_encode.c

--- src/adp4_encode.c
+++ src/adp4_encode.c
@@ -171,12 +171,8 @@ _adp4_encode_sample(adp4_state_t *s_,
   s_->predicted_sample += _adp4_decode_difference(s_->stepsize,encoded_sample);
   s_->predicted_sample = _clamp_s32(s_->predicted_sample,-32768,32767);
 
-  /* compute new stepsize */
-  /* adjust index into stepsize lookup table using new sample */  
   s_->index += g_INDEX_TABLE[encoded_sample];
-  /* check for index underflow|overflow */
   s_->index = _clamp_s32(s_->index,0,STEPSIZE_TABLE_MAX);
-  /* find new quantizer stepsize */  
   s_->stepsize = g_STEPSIZE_TABLE[s_->index]; 
 
   return encoded_sample;
