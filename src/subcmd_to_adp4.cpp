cd05555ca0ae15124ea9da5b3561436fb298e155
Antonio SJ Musumeci <trapexit@spawn.link>
2024-10-12 16:55:09 -0500

    subcmd_to_adp4.cpp

--- src/subcmd_to_adp4.cpp
+++ src/subcmd_to_adp4.cpp
@@ -54,8 +54,12 @@ namespace l
     std::vector<s16> input_data;
     std::vector<u8> output_data;
     u32 sample_count;
+    std::filepath::path output_filepath;
 
+    output_filepath = filepath_ + "adp4.raw"
+    
     in_file  = fopen(filepath_.string().c_str(),"rb");
+    
     out_file = fopen("adp4.test.raw","wb");
 
     sample_count = l::file_size(in_file);
