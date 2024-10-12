3dc9c8115e975113cd41790c4560ed58dfdd33c2
Antonio SJ Musumeci <trapexit@spawn.link>
2024-10-12 18:09:14 -0500

    subcmd_to_adp4.cpp

--- src/subcmd_to_adp4.cpp
+++ src/subcmd_to_adp4.cpp
@@ -73,10 +73,11 @@ namespace l
     // https://3dodev.com/documentation/development/opera/pf25/ppgfldr/mgsfldr/mprfldr/01mpr021
     output_data.resize(((output_data.size() + 3) / 4) * 4);
 
-    fread(&input_data[0],
-          sizeof(s16),
-          input_data.size(),
-          in_file);
+    convert_to_s16be(filepath_, 1, 22050, input_data)
+    // fread(&input_data[0],
+    //       sizeof(s16),
+    //       input_data.size(),
+    //       in_file);
 
     if(encoder_ == "default")
       adp4_encode(input_data.data(),
