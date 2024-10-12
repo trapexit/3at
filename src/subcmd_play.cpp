8cd051540ee2cfb6f51128d176320853d9ae2042
Antonio SJ Musumeci <trapexit@spawn.link>
2024-10-12 15:56:43 -0500

    subcmd_play.cpp

--- src/subcmd_play.cpp
+++ src/subcmd_play.cpp
@@ -15,10 +15,16 @@ namespace l
 
         args.push_back("ffplay");
         args.push_back("-volume");
-        args.push_back("-volume");
-        args.push_back("-volume");
-        args.push_back("-volume");
-        args.push_back("-volume");
+        args.push_back("25");
+        args.push_back("-f");
+        args.push_back("u8");
+        args.push_back("-acodec");
+        args.push_back("adpcm_ima_ws");
+        args.push_back("-ac");
+        args.push_back("1");
+        args.push_back("-ar");
+        args.push_back("22050");
+        args.push_back(filepath.string().c_str());
         
           
           fmt::print("subcmd::play({});\n",
