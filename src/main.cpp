886afe12dfd31a9e5b6524230f7389257b46a0aa
Antonio SJ Musumeci <trapexit@spawn.link>
2024-10-12 18:31:44 -0500

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -36,7 +36,6 @@ generate_to_adp4_argparser(CLI::App      &app_,
     ->check(CLI::IsMember({22050,44100}))
     ->default_val(22050);
 
-
   auto func = std::bind(SubCmd::to_adp4,
                         std::cref(opts));
 
