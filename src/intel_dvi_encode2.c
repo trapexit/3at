48aeb2a390dbb5c06f5a4574ba7b8490f789d359
Antonio SJ Musumeci <trapexit@spawn.link>
2024-10-11 20:43:59 -0500

    intel_dvi_encode2.c

--- src/intel_dvi_encode2.c
+++ src/intel_dvi_encode2.c
@@ -74,7 +74,7 @@ indexTable[INDEX_TABLE_SIZE] =
 static
 const
 u32
-stepsizeTable[STEPSIZE_TABLE_SIZE] = 
+g_stepsize_table[STEPSIZE_TABLE_SIZE] = 
   {
     7, 8, 9, 10, 11, 12, 13, 14, 16, 17,
     19, 21, 23, 25, 28, 31, 34, 37, 41, 45,
