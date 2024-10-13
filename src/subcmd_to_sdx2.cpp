e77851124bceeaf4bf2808bf61040120bd0ae41f
Antonio SJ Musumeci <trapexit@spawn.link>
2024-10-13 16:41:36 -0500

    subcmd_to_sdx2.cpp

--- src/subcmd_to_sdx2.cpp
+++ src/subcmd_to_sdx2.cpp
@@ -120,6 +120,7 @@ SubCmd::to_sdx2(const Opts::ToSDX2 &opts_)
       try
         {
           l::to_sdx2(filepath,
+                     opts_.input_type,
                      opts_.encoder,
                      opts_.output_channels,
                      opts_.output_freq);
