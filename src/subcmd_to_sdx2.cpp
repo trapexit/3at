8835b975c9b26c4b084fae6a42a4787416420361
Antonio SJ Musumeci <trapexit@spawn.link>
2024-10-13 16:31:12 -0500

    subcmd_to_sdx2.cpp

--- src/subcmd_to_sdx2.cpp
+++ src/subcmd_to_sdx2.cpp
@@ -59,7 +59,7 @@ namespace l
     std::vector<s8>  output_data;
     std::filesystem::path output_filepath;
 
-    input_data = l::load_file(filepath_,channels_);
+    input_data = l::load_file(filepath_,channels_,freq_);
     if(input_data.empty())
       throw fmt::exception("failed to load {}",filepath_);
 
